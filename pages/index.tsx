496b7a6297cd3d650f065282d916eefdd2aa74ff
luckyowl157 <owl.faceit@gmail.com>
2023-06-28 19:00:02 +0300

    move viewport and title to index and success pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ export default function Home() {
 
   return (
     <>
+      <title>Registration</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <main>
         <Form />
       </main>
--- pages/success.tsx
+++ pages/success.tsx
@@ -1,15 +1,13 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import ThankYou from '@/components/ThankYou/ThankYou'
 
 //json
 import data from '../public/data/thankyou.json'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
+      <title>Thank You</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <main>
         <ThankYou data={data} />
       </main>
