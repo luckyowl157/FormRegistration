f337273d9ca5764e037f4a722fb82341a11d7395
luckyowl157 <owl.faceit@gmail.com>
2023-06-28 20:34:20 +0300

    added changes

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -86,12 +86,12 @@ export default function Form({ }) {
 
 
   const isPasswordStrong = (password: string): boolean => {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[`!@#$%^&*.,\-_()+=|\\/}{'";:/?<>]).{6,}$/;
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[`!@#$%^&*.,\-_()][+=|\\/}{'";:/?<>]).{6,}$/;
     return regex.test(password);
   };
 
   const validateEmail = (email: string): boolean => {
-    const regex = /^[a-zA-Z0-9!@#$%^&*.,\\\-_()+=|\\/}{'";:/?<>]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/
+    const regex = /^[a-zA-Z0-9!@#$%^&*.,\\\-_()+=|\\/}{'";:/?][<>]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/
     return regex.test(email);
   };
 
